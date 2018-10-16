8c5150af0fe24108729e1091f68ac0364a4e795f
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-16 14:30:29 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -51,12 +51,12 @@ end
 #####################################################
 
 def runner
-  card_total = 0
+  welcome
+  card_total = initial_round
+  card_total == hit?(card_total)
+  display_card_total(card_total)
   until card_total > 21 do
-    welcome
-    card_total == initial_round
-    card_total == hit?(card_total)
-    display_card_total(card_total)
+    runner
   end
-end_game(card_total)
+  end_game(card_total)
 end
